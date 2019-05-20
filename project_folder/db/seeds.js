04f6dc604bbfb5c22482ad7987b18937145e6169
Bek McD <bekahmcdevitt@gmail.com>
2019-05-20 02:13:18 -0400

    seeds file for testing

---
+++ project_folder/db/seeds.js
@@ -0,0 +1,10 @@
+const FlightNumber = require('../models/FlightNumber.js')
+
+const saved = async () => {
+    await FlightNumber.deleteMany()
+    const ATLHSV = new FlightNumber({number: "DL1688", crewMembers: [123456, 789101, 121314], planeType: "717-200", recyclingProduced: 4})
+    await ATLHSV.save()
+    const HSVATL = new FlightNumber({number: "DL2954", crewMembers: [123456, 789101, 121314], planeType: "717-200", recyclingProduced: 5})
+}
+
+saved()
