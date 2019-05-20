df5e44db56feb22bd7929a5946a279199f473833
Bek McD <bekahmcdevitt@gmail.com>
2019-05-20 00:54:49 -0400

    created first model

---
+++ project_folder/models/FlightNumber.js
@@ -0,0 +1,12 @@
+const mongoose = require('../db/connection.js')
+const Schema = mongoose.Schema
+
+const FlightNumber = new Schema({
+    number: String,
+    crewMembers: [Number],
+    planeType: String,
+    recyclingProduced: Number
+    
+})
+
+module.exports = mongoose.model('FlightNumber', FlightNumber) //can space out either of those?
